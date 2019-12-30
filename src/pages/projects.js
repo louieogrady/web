ff8291e6c3b748f4ed6ac8a3887600ef65261bf7
louieogrady <l.ogrady@gmail.com>
2019-12-30 16:44:30 +0000

    adding projects - dynamic pages 5

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,18 +2,17 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Link } from "gatsby"
+import './projects.css';
 
 export default ({ pageContext: { allProjects } }) => (
   <Layout>
     <SEO title="projects" />
-    <div>
+    <div className="projContainer">
         {allProjects.map(project => (
-          <p>
             <Link to={`/projects/${project.name}`}>
               {/* <img src={project.image} alt={project.name} /> */}
-              <p>{project.name}</p>
+              <p className="projText">{project.name}</p>
             </Link>
-          </p>
         ))}
     </div>
   </Layout>
