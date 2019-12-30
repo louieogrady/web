0d559c56cc13ee66790e577982d67192ed3ee517
louieogrady <l.ogrady@gmail.com>
2019-12-30 16:57:37 +0000

    styling projects page

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,12 +1,16 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import './project.css';
 
 export default ({ pageContext: { project } }) => (
-    <div style={{ width: 960, margin: "4rem auto" }}>
-      <h1>{project.name}</h1>
+  <Layout>
+  <SEO title="projects" />
+    <div>
+      <p className='projT'>{project.name}</p>
       {/* <img src={project.image} alt={project.name} /> */}
-      <h3>{project.subInfo}</h3>
-      <h3>{project.description}</h3>
+      <p className='projT2'>{project.subInfo}</p>
+      <p className='projT3'>{project.description}</p>
     </div>
+  </Layout>
   )
