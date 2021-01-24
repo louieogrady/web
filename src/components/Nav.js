2eb6fd758cabef824d7dcaf9e58a70bb218f0881
louieogrady <l.ogrady@gmail.com>
2021-01-24 11:33:49 +0000

    remove some code smell

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,52 +3,63 @@ import github from "../images/Github-Mark-Light-32px.png"
 import linkedin from "../images/linkedin.png"
 import soundcloud from "../images/soundcloud.png"
 import { onMouseOver, onMouseOut } from "../components/functions";
+
+const links = [
+  {
+    name: 'home',
+    path: '/'
+  },
+  {
+    name: 'about',
+    path: '/about'
+  },
+  {
+    name: 'projects',
+    path: '/proj'
+  },
+  {
+    name: 'employment',
+    path: '/employment'
+  },
+  {
+    name: 'education',
+    path: '/education'
+  }
+]
+function navLinks() {
+  return links.map((navItem) => {
+    return <React.Fragment>
+      <a className="nav-link" href={navItem.path}
+        onMouseEnter={event => onMouseOver(event)}
+        onMouseOut={event => onMouseOut(event)}>
+        {navItem.name}
+      </a>
+      <br></br>
+    </React.Fragment>
+  })
+}
 class Nav extends React.Component {
   render() {
     return (
       <React.Fragment>
         <div className="nav-flex-column">
           <nav>
-            <a className="nav-link" href="/" onMouseEnter={event => onMouseOver(event)} onMouseOut={event => onMouseOut(event)}>
-              home
-          </a>
-            <br></br>
-            <a className="nav-link" href="/about" onMouseEnter={event => onMouseOver(event)} onMouseOut={event => onMouseOut(event)}>
-              about
-          </a>
-            <br></br>
-            <a className="nav-link" href="/proj" onMouseEnter={event => onMouseOver(event)} onMouseOut={event => onMouseOut(event)}>
-              projects
-          </a>
-            <br></br>
-            <a className="nav-link" href="/employment" onMouseEnter={event => onMouseOver(event)} onMouseOut={event => onMouseOut(event)}>
-              employment
-          </a>
-            <br></br>
-            <a className="nav-link" href="/education" onMouseEnter={event => onMouseOver(event)} onMouseOut={event => onMouseOut(event)}>
-              education
-            </a>
+            {navLinks()}
             <div className="icon" style={{ "marginTop": "2vh", "display": "flex" }}>
               <div style={{ "maxWidth": "5vh" }}>
-
-                  <a className="social" href="https://www.github.com/louieogrady">
-                    <img style={{ "width": "95%" }} src={github} alt='github-link'></img>
-                  </a>
-
+                <a className="social" href="https://www.github.com/louieogrady">
+                  <img style={{ "width": "95%" }} src={github} alt='github-link'></img>
+                </a>
               </div>
               <div style={{ "maxWidth": "5vh" }}>
-
-                  <a className="social" href="https://www.linkedin.com/in/louieogrady">
-                    <img style={{ "marginTop": "0.35vh", "width": "100%" }} src={linkedin} alt='linkedin-link'></img>
-                  </a>
-
+                <a className="social" href="https://www.linkedin.com/in/louieogrady">
+                  <img className="" style={{ "width": "100%" }} src={linkedin} alt='linkedin-link'></img>
+                </a>
               </div>
               <div style={{ "maxWidth": "5vh" }}>
-
-                  <a className="social" href="https://www.soundcloud.com/muesliboys">
-                    <img style={{ "marginTop": "0.35vh", "width": "100%" }} src={soundcloud} alt='soundcloud-link'></img>
-                  </a>
-
+                <a className="social" href="https://www.soundcloud.com/muesliboys">
+                  <img style={{ "width": "100%" }} src={soundcloud} alt='soundcloud-link'></img>
+                </a>
               </div>
             </div>
           </nav>
