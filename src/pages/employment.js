460ca322e33c05d43a0df9b538e52d24ec9d11d9
louieogrady <l.ogrady@gmail.com>
2021-01-30 17:49:31 +0000

    employment change

--- src/pages/employment.js
+++ src/pages/employment.js
@@ -3,20 +3,48 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { randColor } from "../components/functions"
 
+let employmentData = [
+  {
+    title: 'Junior Frontend Developer',
+    name: 'Thortful Ltd',
+    date: 'June 2019 - Present'
+  },
+  // {
+  //   title: 'Artist',
+  //   name: '',
+  //   date: '2019 - Present',
+  // },
+  // {
+  //   title: 'Audio Editor',
+  //   name: 'Blinkist',
+  //   date: '2019 - Present',
+  // },
+]
+
+function content() {
+  return employmentData.map((x) => {
+    return (
+      <React.Fragment>
+        <div className="text-container">
+          <p style={{ color: randColor() }} className="text">
+            {x.title}
+          </p>
+          <p style={{ color: randColor() }} className="text">
+            {x.name}
+          </p>
+          <p style={{ color: randColor() }} className="text">
+            {x.date}
+          </p>
+        </div>
+      </React.Fragment>
+    )
+  })
+}
+
 const Employment = () => (
   <Layout>
     <SEO title="employment" />
-    <div className="text-container">
-      <p style={{ color: randColor() }} className="text">
-        Thortful Ltd
-      </p>
-      <p style={{ color: randColor() }} className="text">
-        June 2019 - Present
-      </p>
-      <p style={{ color: randColor() }} className="text">
-        Junior Frontend Developer
-      </p>
-    </div>
+    {content()}
   </Layout>
 )
 
